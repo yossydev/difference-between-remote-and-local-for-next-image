b20f74f95ef394537e0918b1d610e4f272046cf5
yossydev <codeyy021@gmail.com>
2024-01-28 11:10:02 +0900

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname:
+          "/yossydev/difference-between-remote-and-local-for-next-image/assets/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
