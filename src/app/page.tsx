71dde25f21d112fad82b1c0fc28a1e683a988215
yossydev <codeyy021@gmail.com>
2024-01-28 15:08:45 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,13 @@ export default function Home() {
       />
       <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        src={"/pexels-mali-maeder-225406.jpg"}
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
         src="https://github.com/yossydev/difference-between-remote-and-local-for-next-image/assets/87469023/2968e824-8640-423c-9c15-d1657d6765df"
         alt="Next.js Logo"
         width={180}
